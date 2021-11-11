c9bf679daf193f9eedb6fe84fb893c72c3c24fb4
Scott G. Ainsworth <scott@ainsworth.us>
2021-11-11 16:49:25 -0500

    Implement unit tests for messaging module
    Closes #10

--- src/bin/avahi-alias/messaging.rs
+++ src/bin/avahi-alias/messaging.rs
@@ -15,11 +15,38 @@ pub fn init(verbose: bool, debug: bool) {
 
 /// Set the log level based on command line arguments
 fn compute_log_level(verbose: bool, debug: bool) -> log::LevelFilter {
-    if verbose {
-        log::LevelFilter::Info
-    } else if debug {
+    if debug {
         log::LevelFilter::Debug
+    } else if verbose {
+        log::LevelFilter::Info
     } else {
         log::LevelFilter::Warn
     }
+}
+
+#[cfg(test)]
+//**********************************************************************************************
+// Unit tests
+//**********************************************************************************************
+
+mod tests {
+
+    use log;
+
+    #[test]
+    fn compute_log_level_yields_warn_as_default() {
+        assert_eq!(super::compute_log_level(false, false), log::LevelFilter::Warn);
+    }
+
+    #[test]
+    fn compute_log_level_yields_info_for_verbose() {
+        assert_eq!(super::compute_log_level(true, false), log::LevelFilter::Info);
+    }
+
+    #[test]
+    fn compute_log_level_yields_debug_for_debug() {
+        assert_eq!(super::compute_log_level(false, true), log::LevelFilter::Debug);
+        assert_eq!(super::compute_log_level(true, true), log::LevelFilter::Debug);
+    }
+
 }
