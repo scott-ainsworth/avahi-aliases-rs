83b5db9f03e74c7fbaaf8c6ba048cdf1ae55edd1
Scott G. Ainsworth <scott@ainsworth.us>
2021-11-25 22:06:15 -0500

    Improve unit tests for Avahi enums.
      - avahi::Interface
      - avahi::Protocol
      - avahi::RecordClass
      - avahi::RecotdType
    
    Issue #40

--- src/avahi_client/protocol.rs
+++ src/avahi_client/protocol.rs
@@ -1,16 +1,17 @@
 #![warn(clippy::all)]
 
-#[derive(Debug, Clone, Copy)]
+#[derive(
+    Debug, Clone, Copy, PartialEq, Eq, num_enum::IntoPrimitive, num_enum::TryFromPrimitive,
+)]
+#[repr(i32)]
 pub enum Protocol {
     /// Protocol not specified (or not needed)
     Unspecified = -1,
 
     /// Internet Protocol v4
-    #[allow(dead_code)]
     IPv4        = 0,
 
     /// Internet Protocol v6
-    #[allow(dead_code)]
     IPv6        = 1,
 }
 
@@ -19,4 +20,50 @@ impl dbus::arg::Append for Protocol {
     fn append(self, ia: &mut dbus::arg::IterAppend<'_>) { ia.append(self as i32); }
     #[inline(always)]
     fn append_by_ref(&self, ia: &mut dbus::arg::IterAppend<'_>) { ia.append(*self as i32); }
+}
+
+//**********************************************************************************************
+// Unit Tests
+//**********************************************************************************************
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    static TEST_DATA: &[(Protocol, i32)] =
+        &[(Protocol::Unspecified, -1), (Protocol::IPv4, 0), (Protocol::IPv6, 1)];
+
+    #[test]
+    fn constants_are_correct() {
+        for datum in TEST_DATA {
+            assert_eq!(datum.1, datum.0.into())
+        }
+    }
+
+    #[test]
+    fn try_from_converts_valid_values_to_correct_variant() {
+        for datum in TEST_DATA {
+            let ok = Protocol::try_from(datum.1);
+            eprintln!("try_converts_valid_value_to_correct_variant :: err={:?}", ok);
+            assert_eq!(datum.0, Protocol::try_from(datum.1).unwrap())
+        }
+    }
+
+    #[test]
+    fn try_from_returns_error_for_invalid_values() {
+        for value in [-2, 2] {
+            let err = Protocol::try_from(value);
+            eprintln!("try_from_returns_error_for_invalid_values :: err={:?}", err);
+            assert!(Protocol::try_from(value).is_err());
+        }
+    }
+
+    #[test]
+    fn error_has_correct_message() {
+        eprintln!(":: {}", Protocol::try_from(99).unwrap_err());
+        assert_eq!(
+            format!("{}", Protocol::try_from(99).unwrap_err()),
+            "No discriminant in enum `Protocol` matches the value `99`"
+        );
+    }
 }
--- src/avahi_client/record_type.rs
+++ src/avahi_client/record_type.rs
@@ -1,42 +1,37 @@
 #![warn(clippy::all)]
-#[derive(Debug, Clone, Copy)]
+
+#[derive(
+    Debug, Clone, Copy, PartialEq, Eq, num_enum::IntoPrimitive, num_enum::TryFromPrimitive,
+)]
+#[repr(u16)]
 pub enum RecordType {
     /// IPv4 Address record
-    #[allow(dead_code)]
     A     = 1,
 
     /// Name Server record
-    #[allow(dead_code)]
     Ns    = 2,
 
     /// Canonical Name record
     Cname = 5,
 
     /// Start of Authority record
-    #[allow(dead_code)]
     Soa   = 6,
     /// Pointer (reverse lookup) record
-    #[allow(dead_code)]
     Ptr   = 12,
 
     /// Host Informattion record
-    #[allow(dead_code)]
     Hinfo = 13,
 
     /// Mail Exchanger record
-    #[allow(dead_code)]
     Mx    = 15,
 
     /// Text record
-    #[allow(dead_code)]
     Txt   = 16,
 
     /// IPv6 address record
-    #[allow(dead_code)]
     Aaa   = 28,
 
     /// Service record
-    #[allow(dead_code)]
     Srv   = 33,
 }
 
@@ -45,4 +40,59 @@ impl dbus::arg::Append for RecordType {
     fn append(self, ia: &mut dbus::arg::IterAppend<'_>) { ia.append(self as u16); }
     #[inline(always)]
     fn append_by_ref(&self, ia: &mut dbus::arg::IterAppend<'_>) { ia.append(*self as u16); }
+
+//**********************************************************************************************
+// Unit Tests
+//**********************************************************************************************
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    static TEST_DATA: &[(RecordType, u16)] = &[
+        (RecordType::A, 1),
+        (RecordType::Ns, 2),
+        (RecordType::Cname, 5),
+        (RecordType::Soa, 6),
+        (RecordType::Ptr, 12),
+        (RecordType::Hinfo, 13),
+        (RecordType::Mx, 15),
+        (RecordType::Txt, 16),
+        (RecordType::Aaa, 28),
+        (RecordType::Srv, 33),
+    ];
+
+    #[test]
+    fn constants_are_correct() {
+        for datum in TEST_DATA {
+            assert_eq!(datum.1, datum.0.into())
+        }
+    }
+
+    #[test]
+    fn try_from_converts_valid_values_to_correct_variant() {
+        for datum in TEST_DATA {
+            let ok = RecordType::try_from(datum.1);
+            eprintln!("try_converts_valid_value_to_correct_variant :: err={:?}", ok);
+            assert_eq!(datum.0, RecordType::try_from(datum.1).unwrap())
+        }
+    }
+
+    #[test]
+    fn try_from_returns_error_for_invalid_values() {
+        for value in [0, 3, 4, 7, 8, 11, 14, 17, 27, 34] {
+            let err = RecordType::try_from(value);
+            eprintln!("try_from_returns_error_for_invalid_values :: err={:?}", err);
+            assert!(RecordType::try_from(value).is_err());
+        }
+    }
+
+    #[test]
+    fn error_has_correct_message() {
+        eprintln!(":: {}", RecordType::try_from(99).unwrap_err());
+        assert_eq!(
+            format!("{}", RecordType::try_from(99).unwrap_err()),
+            "No discriminant in enum `RecordType` matches the value `99`"
+        );
+    }
 }
